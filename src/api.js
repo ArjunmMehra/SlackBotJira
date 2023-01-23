3c94294cedec2b1b1a32858a872a804304c10f19
Gaurav Sharma <gaurav.sharma21@globallogic.com>
2023-01-23 16:55:29 +0530

    added message templates

--- src/api.js
+++ src/api.js
@@ -5,10 +5,19 @@ const app = express();
 const router = express.Router();
 // hosted URL  : http://localhost:9000/.netlify/functions/api
 router.post("/", (req, res) => {
-console.log(req);
-  res.json({
-    hello: "hi!"
-  });
+    const data = req;
+    var callbackId = data.callback_id;
+    var actionValue = data.actions[0].value;
+    if (callbackId === `${constants.TICKET_LIST_SELECT}`) {
+        if (actionValue === "option1") {
+            bot.postMessage(data.channel, 'You selected option 1, this is what I will do...');
+        } else if (actionValue === "option2") {
+            bot.postMessage(data.channel, 'You selected option 2, this is what I will do...');
+        } else if (actionValue === "option3") {
+            bot.postMessage(data.channel, 'You selected option 3, this is what I will do...');
+        }
+    }
+   return data;
 });
 
 app.use(`/.netlify/functions/api`, router);
