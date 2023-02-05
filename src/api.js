422cbbae5fd1e63d2d0e26299f9cfbf27437bdf2
Arjun Singh <arjun.singh@globallogic.com>
2023-02-05 14:39:31 +0530

    resolved axios issue

--- src/api.js
+++ src/api.js
@@ -8,12 +8,60 @@ const jiraService = require("/services/JiraServices");
 const config = setupConfig();
 const bot = setupSlackBot(config);
 require("dotenv").config();
+const axios = require("axios");
 
 const { WebClient } = require("@slack/web-api");
 const { channel } = require("slack-block-builder");
 
+
+const JIRA_URL = process.env.JIRA_URL;
+const JIRA_USERNAME = process.env.USER;
+const JIRA_TOKEN = process.env.JIRA_TOKEN;
+const createEndPoint = "rest/api/3/issue";
+
+const axiosConfig = {
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+    Authorization:
+      "Basic " +
+      Buffer.from(JIRA_USERNAME + ":" + JIRA_TOKEN).toString("base64"),
+  },
+};
+
 const app = express();
 const router = express.Router();
+
+const createData = (payload) => {
+  const { summary, description, issueType } = payload;
+  return JSON.stringify({
+    fields: {
+      project: {
+        id: "10000",
+      },
+      summary: `${summary}`,
+      description: {
+        type: "doc",
+        version: 1,
+        content: [
+          {
+            type: "paragraph",
+            content: [
+              {
+                type: "text",
+                text: `${description}`,
+              },
+            ],
+          },
+        ],
+      },
+      labels: [`${issueType}`],
+      issuetype: {
+        id: "10001",
+      },
+    },
+  });
+};
 // hosted URL  : http://localhost:9000/.netlify/functions/api
 router.post("/", async (req, res) => {
   if (req.apiGateway.event.body) {
@@ -43,8 +91,14 @@ router.post("/", async (req, res) => {
         issueType,
       };
       console.log("payload", payload);
-      const result = await jiraService.createTicket(payload);
-      bot.postMessage(channel, slackMessageBuilders.createdMessage(), result);
+      //const result = await jiraService.createTicket(payload);
+      const response = await axios.post(
+        JIRA_URL + createEndPoint,
+        createData(payload),
+        axiosConfig
+      );
+      console.log(response);
+      bot.postMessage(channel, slackMessageBuilders.createdMessage(), response);
       res.status(200).end();
       console.log("after end");
     }
